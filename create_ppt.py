a2704d83cfba6c8becc8c51f90b337eb8519bd48
Tim Freeman <d.timothy.freeman@gmail.com>
2018-02-18 15:54:38 -0500

    automated process to hit api, get data, make ppts, and send the email

--- create_ppt.py
+++ create_ppt.py
@@ -1,37 +1,122 @@
+import os
+
+from apiclient import discovery
+import httplib2
+
 from pptx import Presentation
 from pptx.util import Inches
-import csv
 
-studentdata = csv.reader(open('PWAverage.csv'))
-prs = Presentation('test.pptx')
-blank_slide_layout = prs.slide_layouts[6]
-slide = prs.slides.add_slide(blank_slide_layout)
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.encoders import encode_base64
 
-r = 0
-placeholder = 0
-left = 0.5
-top = 0.5
-width = 1
-height = 1
+from credentials import get_credentials
 
-for row in studentdata:
-	if r > 0:
-		if placeholder == 42:
-			slide = prs.slides.add_slide(blank_slide_layout)
-			placeholder = 0
-			left = 0.5
-			top = 0.5
-		if placeholder == 14 or placeholder == 28:
-			left = left + 3
-			top = 0.5
-		
-		first_name = row[1]
-		last_name = row[2]
-		pwa = row[4]
-		textBox = slide.shapes.add_textbox(Inches(left),Inches(top),Inches(width),Inches(height))
-		textBox.text_frame.text = first_name  + last_name + " - " + pwa
-		placeholder = placeholder+1
-		top = top+0.5
-	r = r+1
+from secrets import FROM_EMAIL, FROM_PASSWORD, TO_EMAIL, SHEET_ID
 
-prs.save('studentTest.pptx')
+def convert_file_to_attachment(file):
+	file_name = file.split('/')[-1]
+	file_object = open(file, 'rb')
+	attachment = MIMEBase('application', 'octet-stream')
+	attachment.set_payload((file_object).read())
+	encode_base64(attachment)
+	attachment.add_header('Content-Disposition', "attachment; filename= %s" % file_name)
+	return attachment
+
+def send_email_with_attachments(files_to_attach):
+	message = MIMEMultipart()
+	message['From'] = FROM_EMAIL
+	message['To'] = TO_EMAIL
+	message['Subject'] = 'This Week\'s Community Meeting Powerpoints'
+
+	for file in files_to_attach:
+		attachment = convert_file_to_attachment(file)
+		message.attach(attachment)
+
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(FROM_EMAIL, FROM_PASSWORD)
+	text = message.as_string()
+	server.sendmail(FROM_EMAIL, TO_EMAIL, text)
+	server.quit()
+
+def proccess_tabs(tabs, service, spreadsheetId):
+	parsed_tabs = []
+
+	for tab in tabs:
+		tab_name = tab.get('properties', {}).get('title')
+		rangeName = tab_name + '!A2:C'
+		result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
+		student_data = result.get('values', [])
+		file_path = make_path(tab_name)
+		tab_object = {'file': file_path, 'student_data': student_data}
+		parsed_tabs.append(tab_object)
+
+	return parsed_tabs
+
+def make_path(tab_name):
+	return os.getcwd() + '/' + tab_name + '.pptx'
+
+def make_all_ppts(parsed_tabs):
+	for tab in parsed_tabs:
+		file_path = tab['file']
+		student_data = tab['student_data']
+		make_ppt(file_path, student_data)
+
+def make_ppt(file_path, student_data):
+	presentation = Presentation()
+	blank_slide_layout = presentation.slide_layouts[6]
+	slide = presentation.slides.add_slide(blank_slide_layout)
+
+	r = 0
+	placeholder = 0
+	left = 0.5
+	top = 1
+	width = 1
+	height = 1
+
+	for row in student_data:
+		if r > 0:
+			if placeholder == 34:
+				slide = presentation.slides.add_slide(blank_slide_layout)
+				placeholder = 0
+				left = 0.5
+				top = 1
+			if placeholder == 12 or placeholder == 24:
+				left = left + 3
+				top = 1
+			first_name = row[0]
+			last_name = row[1]
+			pwa = row[2]
+			textBox = slide.shapes.add_textbox(Inches(left),Inches(top),Inches(width),Inches(height))
+			textBox.text_frame.text = first_name + ' ' + last_name + ' ' + pwa
+			placeholder = placeholder+1
+			top = top+0.5
+		r = r+1
+
+	presentation.save(file_path)
+
+def isolate_file_names_for_attachments(parsed_tabs):
+	file_names = []
+	
+	for tab in parsed_tabs:
+		file_names.append(tab['file'])
+
+	return file_names
+
+def main():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?''version=v4')
+    service = discovery.build('sheets', 'v4', http=http, discoveryServiceUrl=discoveryUrl)  
+    tabs = service.spreadsheets().get(spreadsheetId=SHEET_ID).execute().get('sheets', '')
+    processed_tabs = proccess_tabs(tabs, service, SHEET_ID)
+    make_all_ppts(processed_tabs)
+    files = isolate_file_names_for_attachments(processed_tabs)
+    send_email_with_attachments(files)
+
+
+if __name__ == '__main__':
+    main()
---
+++ credentials.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+import os
+
+from secrets import SECRET_JSON
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/sheets.googleapis.com-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+CLIENT_SECRET_FILE = SECRET_JSON
+APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
